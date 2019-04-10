8288096ab606ed307318d693aebcaf17d195d49d
rahim <chetibi.abderrahim@gmail.com>
2019-04-10 02:09:29 +0100

    Fix 6.61 LME launcher address patch

--- CUSTOM_FIRMWARES/ME/mecfw/launch/kernel_patch.h
+++ CUSTOM_FIRMWARES/ME/mecfw/launch/kernel_patch.h
@@ -19,6 +19,13 @@ int MemlmdPatchAddr_02g[] =		{ 0x00001078, 0x00001240, 0x00001294, 0x00000134, 0
 int MemlmdPatchAddr_01g[] =		{ 0x00001070, 0x00001238, 0x0000128C, 0x0000020C, 0x00000F70, 0x00000FD4 };
 int MemlmdPatchAddr_02g[] =		{ 0x000010F8, 0x000012C0, 0x00001314, 0x0000020C, 0x00000FF8, 0x0000105C };
 
+#elif _PSP_FW_VERSION == 661
+#define sceKernelLoadModuleUser_Call1				0x00000760
+#define sceKernelLoadModuleUser_Call2				0x000007C0
+
+int MemlmdPatchAddr_01g[] =		{ 0x00001070, 0x00001238, 0x0000128C, 0x0000020C, 0x00000F70, 0x00000FD4 };
+int MemlmdPatchAddr_02g[] =		{ 0x000010F8, 0x000012C0, 0x00001314, 0x0000020C, 0x00000FF8, 0x0000105C };
+
 #else
 #error kernel_patch.h
 #endif
